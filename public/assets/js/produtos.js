9b8bcf666ff4e5a64fc1e88891e4d86c3a83edde
Arthuz2 <arthur.porcino43@gmail.com>
2026-06-24 16:47:14 -0300

    chore(produtos.js): Limpar comentários e linhas não utilizadas no arquivo de produtos.
    Remover comentários e linhas não utilizadas no arquivo de produtos.

--- public/assets/js/produtos.js
+++ public/assets/js/produtos.js
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
-function listarProdutos(callback) {
-  request("/produtos/", "GET", null, callback);
-}
-
-function criarProduto(produto, callback) {
-  request("/produtos/", "POST", produto, callback);
-}
-
-function editarProduto(id, produto, callback) {
-  request("/produtos/" + id, "PUT", produto, callback);
-}
-
-function excluirProdutoAPI(id, callback) {
-=======
 import { request } from './api.js'
 
 export function listarProdutos(callback) {
@@ -28,6 +13,5 @@ export function editarProduto(id, produto, callback) {
 }
 
 export function excluirProdutoAPI(id, callback) {
->>>>>>> Stashed changes
   request("/produtos/" + id, "DELETE", null, callback);
 }
