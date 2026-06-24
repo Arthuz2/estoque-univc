25112eb32f5a0798a892466abdce6607cf07af6e
Guilherme Vagmaker Moreira <Guilhermevag098@gmail.com>
2026-06-24 17:29:15 -0300

    versão teste aqui

--- public/assets/js/produtos.js
+++ public/assets/js/produtos.js
@@ -1,17 +1,17 @@
-import { request } from './api.js'
+import { request } from "./api.js";
 
-export function listarProdutos(callback) {
-  request("/produtos/", "GET", null, callback);
+export function listarProdutos(callback, errorCallback) {
+  request("/produtos/", "GET", null, callback, errorCallback);
 }
 
-export function criarProduto(produto, callback) {
-  request("/produtos/", "POST", produto, callback);
+export function criarProduto(produto, callback, errorCallback) {
+  request("/produtos/", "POST", produto, callback, errorCallback);
 }
 
-export function editarProduto(id, produto, callback) {
-  request("/produtos/" + id, "PUT", produto, callback);
+export function editarProduto(id, produto, callback, errorCallback) {
+  request("/produtos/" + id, "PUT", produto, callback, errorCallback);
 }
 
-export function excluirProdutoAPI(id, callback) {
-  request("/produtos/" + id, "DELETE", null, callback);
+export function excluirProdutoAPI(id, callback, errorCallback) {
+  request("/produtos/" + id, "DELETE", null, callback, errorCallback);
 }
